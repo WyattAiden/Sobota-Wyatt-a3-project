90835534df4136b7b9e8e50a56f424ed3bd098b1
WyattAiden <wyatt.sobota@mohawkcollege.ca>
2024-11-09 21:19:35 -0500

    card color change

--- Sobota-Wyatt-A3-project/Card.cs
+++ Sobota-Wyatt-A3-project/Card.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Numerics;
+using System.Threading;
 
 namespace Game10003
 {
@@ -9,20 +10,60 @@ namespace Game10003
         private float H;
         public float X;
         public float Y;
+        public Color FrontColor; // The color displayed on the front of the card
 
-
-        public Card()
+        public Card(float xpos,float ypos, Color color)
         {
-            W = 50;
+            W = 40;
             H = 80;
-            
+            X = xpos;
+            Y = ypos;
+            FrontColor = color; // Assign color to the card
         }
 
-
-        private void CardFront()
+        public Card(int x, int y)
         {
-            Draw.FillColor = Color.Yellow;
-            Draw.Rectangle(X, Y, W, H);
+            X = x;
+            Y = y;
+        }
+
+        bool CardFaceDown = true;
+        public void Update()
+        {
+            // Check if the left mouse button is pressed and if the mouse is over this card
+            if (Input.IsMouseButtonPressed(MouseInput.Left) && IsMouseTouchingBox())
+            {
+                // Flip the card when clicked
+                CardFaceDown = !CardFaceDown;
+            }
+
+            // Draw the card based on its current state
+            if (CardFaceDown)
+                CardBack();
+            else
+                CardFront(X, Y);
+        }
+
+        private bool IsMouseTouchingBox()
+        {
+            // Get the current mouse position
+            Vector2 mousePos = Input.GetMousePosition();
+
+            // Calculate the edges of the card
+            float leftEdge = X;
+            float rightEdge = X + W;
+            float topEdge = Y;
+            float bottomEdge = Y + H;
+
+            // Check if the mouse position is within the card's boundaries
+            return mousePos.X >= leftEdge && mousePos.X <= rightEdge &&
+                   mousePos.Y >= topEdge && mousePos.Y <= bottomEdge;
+        }
+
+        private void CardFront(float Cardx,float Cardy)
+        {
+            Draw.FillColor = FrontColor;
+            Draw.Rectangle(Cardx, Cardy, W, H);
         }
         private void CardBack()
         {
--- Sobota-Wyatt-A3-project/Game.cs
+++ Sobota-Wyatt-A3-project/Game.cs
@@ -1,6 +1,8 @@
 ﻿// Include code libraries you need below (use the namespace).
 using System;
+using System.Collections.Generic;
 using System.Numerics;
+using System.Reflection;
 
 // The namespace your code is in.
 namespace Game10003
@@ -11,15 +13,54 @@ namespace Game10003
     public class Game
     {
         // Place your variables here:
+        Deck PlaceDeck;
 
-
-        /// <summary>
-        ///     Setup runs once before the game loop begins.
-        /// </summary>
+        Vector2[] Positions = new Vector2[12];
+        Card[] cards = new Card[12];
         public void Setup()
         {
             Window.SetTitle("Concentration");
             Window.SetSize(400, 400);
+
+            // Define the 6 colors and duplicate them to have pairs
+            List<Color> colors = new List<Color>
+            {
+                Color.Blue, Color.Green, Color.Yellow, Color.Cyan, Color.Magenta, Color.Black
+            };
+
+            // Duplicate each color to make pairs
+            List<Color> colorPairs = new List<Color>();
+            foreach (Color color in colors)
+            {
+                colorPairs.Add(color);
+                colorPairs.Add(color);
+            }
+
+            // Shuffle the list of colors
+            System.Random rand = new System.Random();
+            for (int i = 0; i < colorPairs.Count; i++)
+            {
+                int randomIndex = rand.Next(i, colorPairs.Count);
+                Color temp = colorPairs[i];
+                colorPairs[i] = colorPairs[randomIndex];
+                colorPairs[randomIndex] = temp;
+            }
+
+            //making the aray to make the cards
+            for (int i = 0; i < 4; i++)
+            {
+                int x = i * 70 + 20;
+                for (int j = 0; j < 3; j++)
+                {
+                    int y = j * 100 + 50;
+                    int inex = i + j * 4;
+                    Positions[inex] = new Vector2(x, y);
+                    cards[inex] = new Card(x,y);
+                    cards[inex] = new Card(x, y, colorPairs[inex]);
+                }
+            }
+            
+            PlaceDeck = new Deck();
         }
 
         /// <summary>
@@ -28,9 +69,12 @@ namespace Game10003
         public void Update()
         {
             Window.ClearBackground(Color.White);
-
-
             
+            foreach (Card card in cards)
+            {
+                card.Update();
+            }
+            PlaceDeck.Update();
         }
     }
 }
